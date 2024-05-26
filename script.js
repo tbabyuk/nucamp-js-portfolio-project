a6e79233cd351223560d5c4347b623673cd6ca70
Terry Babyuk <tbabyuk@gmail.com>
2024-05-26 00:41:51 -0400

    cleaned up code inside showNote(), made form disappear after all notes were presented and 'View Results' button appear instead

---
+++ script.js
@@ -0,0 +1,89 @@
+const noteImage = document.getElementById("note_img");
+const answers = document.querySelectorAll(".answer");
+const answerOptions = document.querySelectorAll(".radio_btn");
+const answerLabels = document.querySelectorAll(".note_label");
+const form = document.getElementById("answers_form");
+const submitBtn = document.getElementById("submit_btn");
+const resultsBtn = document.getElementById("results_btn");
+
+
+class Note {
+    constructor(noteImage, noteName, answerOptions) {
+        this.noteImage = noteImage;
+        this.noteName = noteName;
+        this.answerOptions = answerOptions;
+    }
+}
+
+
+const noteA = new Note("/assets/treble-note-a.png", "A", ["A", "D", "C"]);
+const noteB = new Note("/assets/treble-note-b.png", "B", ["G", "B", "D"]);
+const noteC = new Note("/assets/treble-note-c.png", "C", ["C", "F", "E"]);
+const noteD = new Note("/assets/treble-note-d.png", "D", ["D", "A", "F"]);
+const noteE = new Note("/assets/treble-note-e.png", "E", ["C", "E", "G"]);
+const noteF = new Note("/assets/treble-note-f.png", "F", ["D", "C", "F"]);
+const noteG = new Note("/assets/treble-note-g.png", "G", ["G", "B", "A"]);
+
+
+const notesArray = [noteA, noteB, noteC, noteD, noteE, noteF, noteG];
+
+const notesDisplayed = [];
+
+showNote(notesArray);
+
+
+function showNote(array) {
+
+    if(notesDisplayed.length === array.length) {
+        console.log("All notes have been displayed!")
+        form.style.display = "none";
+        resultsBtn.style.display = "block";
+        return;
+    }
+
+    const randomNoteIndex = Math.floor(Math.random() * array.length);
+    const currentNote = array[randomNoteIndex];
+
+
+    if(!notesDisplayed.includes(currentNote.noteName)) {
+
+        noteImage.src=currentNote.noteImage;
+
+        notesDisplayed.push(currentNote.noteName);
+
+        answerOptions.forEach((option, index) => {
+            option.value = currentNote.answerOptions[index];
+            const associatedLabel = option.nextElementSibling;
+            associatedLabel.innerText = currentNote.answerOptions[index];
+        })
+    } else {
+        showNote(notesArray);
+    }
+}
+
+
+form.addEventListener("submit", recordUserAnswer);
+
+
+const userAnswersArray = [];
+
+function recordUserAnswer(e) {
+    e.preventDefault();
+    if (userAnswersArray.length === 7) {
+        showResults();
+        return;
+    }
+    // const selectedOption = form[]
+    const userSelection = form.querySelector('input[type="radio"]:checked');
+    // console.log("logging user answer", userSelection.value);
+    userAnswersArray.push(userSelection.value);
+    console.log("userAnswersArray:", userAnswersArray);
+    showNote(notesArray);
+}
+
+
+function showResults() {
+    console.log("show Results fired", userAnswersArray);
+}
+
+// function to check answer and hold answers in an array
